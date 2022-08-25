c1474087a863f71650d63ad5695aa318a789e8bf
seus31 <us.balut.big.dipper@gmail.com>
2022-08-26 00:14:35 +0900

    不要な記述削除

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,21 +7,6 @@ router.get('/', function (req, res, next) {
   } else {
       res.redirect('/login');
   }
-  const client = require("../db_client").pg_client()
-
-  client.connect()
-    .then(() => console.log("Connected successfuly"))
-    .then(() => client.query("select * from users"))
-    .then(function (results) {
-      console.table(results.rows)
-      res.render('index', { title: 'セレクト', sql_result: results.rows})
-    })
-    .catch((e => console.log(e)))
-    .finally((() => client.end()))
-
-  // res.render('index', { title: 'Express' ,sql_result: results.rows});
-
-
 });
 
 module.exports = router;
