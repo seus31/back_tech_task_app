84fecd3cf571088ddb0b8ac8232afb6369d167de
seus31 <us.balut.big.dipper@gmail.com>
2022-08-26 12:15:15 +0900

    sequelize追加

--- init-sql/00-test.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-  id SERIAL NOT NULL,
-  username varchar(256),
-  email varchar(256),
-  password varchar(256),
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
-  PRIMARY KEY (id)
-);
